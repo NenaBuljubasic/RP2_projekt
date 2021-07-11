f3b512a940f81916f5bb5e583c9ecf15112c4db8
NenaBuljubasic <80649803+NenaBuljubasic@users.noreply.github.com>
2021-07-11 23:13:21 +0200

    Update login_index.php

--- view/login_index.php
+++ view/login_index.php
@@ -1,12 +1,11 @@
 <?php require_once __SITE_PATH . '/view/_header.php'; ?>
 
-<<<<<<< HEAD
+
 <form class="login" method="post" action="<?php echo __SITE_URL . '/index.php?rt=login/korisnik'?>">
 	<!--<div class="wrapper">-->
 =======
 <form class="login" method="post" action="<?php echo __SITE_URL . '/index.php?rt=user/login'?>">
 	<div class="wrapper">
->>>>>>> e64e6ed62a7211c4bbfc166936da11398bba8d81
 	Username: 
 	<input type="text" name="username" />
 	<br />
