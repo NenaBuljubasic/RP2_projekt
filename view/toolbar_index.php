2eefbd9c2431131495ffcf76ff31fb04f39679df
NenaBuljubasic <80649803+NenaBuljubasic@users.noreply.github.com>
2021-07-13 15:41:33 +0200

    Update toolbar_index.php

--- view/toolbar_index.php
+++ view/toolbar_index.php
@@ -3,7 +3,7 @@
 
 <ul>
 
-<li><a href="<?php echo __SITE_URL . '/index.php?rt=user/login'?>">Lista rezervacija</a></li>
+<li><a href="<?php echo __SITE_URL . '/index.php?rt=user/show'?>">Lista rezervacija</a></li>
 <li><a href="<?php echo __SITE_URL . '/index.php?rt=reservation/reserve'?>">Rezerviraj</a></li>
 <li><a href="<?php echo __SITE_URL  .'/index.php?rt=user/logout'?>">Odjavi se</a></li>
 </ul>
