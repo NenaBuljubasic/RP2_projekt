a3df14ed17d6801a43545fa75a5a5a7a7e1c52eb
Gabrijela Zlatunić <85293611+gzlatunic@users.noreply.github.com>
2021-07-13 10:50:54 +0200

    Add files via upload
    Controller za nelogirane korisnike

---
+++ controller/guestController.php
@@ -0,0 +1,13 @@
+<?php 
+
+class GuestController extends BaseController
+{
+	public function index() 
+	{  
+        require_once __DIR__ . '/../view/Guest.php';
+
+        // popravi ovo da radi
+    }
+}
+
+?>
