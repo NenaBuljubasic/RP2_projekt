7d9a444dfd2483524d3a5e467e1fbdf486ef7217
NenaBuljubasic <80649803+NenaBuljubasic@users.noreply.github.com>
2021-07-14 19:59:12 +0200

    css

--- view/toolbar_index.php
+++ view/toolbar_index.php
@@ -1,9 +1,5 @@
-
-
-
-<ul>
-
-<li><a href="<?php echo __SITE_URL . '/index.php?rt=user/show'?>">Lista rezervacija</a></li>
-<li><a href="<?php echo __SITE_URL . '/index.php?rt=reservation/reserve'?>">Rezerviraj</a></li>
-<li><a href="<?php echo __SITE_URL  .'/index.php?rt=user/logout'?>">Odjavi se</a></li>
-</ul>
+<nav>
+    <a href="<?php echo __SITE_URL . '/index.php?rt=user/show'?>">Lista rezervacija</a>
+    <a href="<?php echo __SITE_URL . '/index.php?rt=reservation/reserve'?>">Rezerviraj</a>
+    <a href="<?php echo __SITE_URL  .'/index.php?rt=user/logout'?>">Odjavi se</a>
+</nav>
